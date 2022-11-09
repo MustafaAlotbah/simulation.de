a113dd978755686f10064b4ea50e69405a11c966
Mustafa Alotbah <mustafa.alotbah@gmail.com>
2022-11-09 07:51:35 +0100

    Spring-Mass System: * Forces: minor

--- spring_mass_01/simulation/forces.py
+++ spring_mass_01/simulation/forces.py
@@ -15,14 +15,15 @@ def apply_earth_gravity(particle: Particle):
 
 # Gravitational Forces in Newtonian Mechanics
 def apply_gravitational_forces(planet1: Particle, planet2: Particle):
-    gravity = - gravitational_constant * planet1.mass * planet2.mass / np.linalg.norm(planet1.position - planet2.position) ** 3
-    gravitational_force = gravity * (planet1.position - planet2.position)
+    if planet1.apply_gravitational_forces or planet2.apply_gravitational_forces:
+        gravity = - gravitational_constant * planet1.mass * planet2.mass / np.linalg.norm(planet1.position - planet2.position) ** 3
+        gravitational_force = gravity * (planet1.position - planet2.position)
 
-    if planet1.apply_gravitational_forces:
-        planet1.force += gravitational_force
+        if planet1.apply_gravitational_forces:
+            planet1.force += gravitational_force
 
-    if planet2.apply_gravitational_forces:
-        planet2.force -= gravitational_force
+        if planet2.apply_gravitational_forces:
+            planet2.force -= gravitational_force
 
 
 def apply_spring_forces(spring: Spring):
